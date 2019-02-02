f6e64a2c8afb93d9fb081f897ecc2a391e5a6cbb
Ted Velkoff <ted.velkoff@ionep.com>
2019-02-01 23:25:19 -0500

    Add EntityScan annotation
    Since the domain classes (entities) are in a package from current, they are not found automatically by Spring.
    This ensures that the entity classes will be recognized.

--- src/main/java/com/ionep/egis/history/HistoryApplication.java
+++ src/main/java/com/ionep/egis/history/HistoryApplication.java
@@ -2,8 +2,10 @@ package com.ionep.egis.history;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.ionep.egis.current.domain")
 public class HistoryApplication {
 
 	public static void main(String[] args) {
