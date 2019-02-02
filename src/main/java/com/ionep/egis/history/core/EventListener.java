91d3bc9a524b485486b61ac56ff6150de7435dbf
Ted Velkoff <ted.velkoff@ionep.com>
2019-02-01 23:26:44 -0500

    Adds call to writer as events are received from Kafka

--- src/main/java/com/ionep/egis/history/core/EventListener.java
+++ src/main/java/com/ionep/egis/history/core/EventListener.java
@@ -4,10 +4,12 @@ import java.util.concurrent.CountDownLatch;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import com.ionep.egis.current.domain.CurrentConditions;
+import com.ionep.egis.history.service.CurrentConditionsWriter;
 
 @Component
 public class EventListener {
@@ -15,11 +17,15 @@ public class EventListener {
 	private static Log log = LogFactory.getLog(EventListener.class);
 	
 	private CountDownLatch currentConditionsLatch = new CountDownLatch(1);
+	
+	@Autowired
+	private CurrentConditionsWriter currentConditionsWriter;
 
 	@KafkaListener(topics = "BALTIMORE_MD", containerFactory = "currentConditionsKafkaListenerContainerFactory")
 	public void currentConditionsListener(CurrentConditions currentConditions) {
 		log.info("Received message: " + currentConditions);
+		this.currentConditionsWriter.writeCurrentConditions(currentConditions);
 		this.currentConditionsLatch.countDown();
 	}
-
+	
 }
